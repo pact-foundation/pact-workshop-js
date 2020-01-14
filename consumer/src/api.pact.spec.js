c181863322eafd7bed806e3a67547a4d2bf94f5e
beng.lim@latitudefinancial.com <beng.lim@latitudefinancial.com>
2020-01-15 00:24:24 +1100

    added step6

--- consumer/src/api.pact.spec.js
+++ consumer/src/api.pact.spec.js
@@ -60,6 +60,33 @@ describe("API Pact test", () => {
                 {"id": "09", "name": "Gem Visa", "type": "CREDIT_CARD"}
             ]);
         });
+
+        test("no products exists", async () => {
+
+            // set up Pact interactions
+            await provider.addInteraction({
+                state: 'no products exist',
+                uponReceiving: 'get all products',
+                withRequest: {
+                    method: 'GET',
+                    path: '/products'
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8'
+                    },
+                    body: []
+                },
+            });
+
+            let api = new API(provider.mockService.baseUrl);
+
+            // make request to Pact mock server
+            let product = await api.getAllProducts();
+
+            expect(product).toStrictEqual([]);
+        });
     });
 
     describe("getting one product", () => {
@@ -96,6 +123,27 @@ describe("API Pact test", () => {
                 type: "CREDIT_CARD",
                 name: "28 Degrees"
             });
+        });
+
+        test("product does not exist", async () => {
+
+            // set up Pact interactions
+            await provider.addInteraction({
+                state: 'product with ID 11 does not exist',
+                uponReceiving: 'get product with ID 11',
+                withRequest: {
+                    method: 'GET',
+                    path: '/product/11'
+                },
+                willRespondWith: {
+                    status: 404
+                },
+            });
+
+            let api = new API(provider.mockService.baseUrl);
+
+            // make request to Pact mock server
+            await expect(api.getProduct("11")).rejects.toThrow("Request failed with status code 404");
         });
     });
 });
