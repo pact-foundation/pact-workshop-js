e3067747bfb080decaa2d9c9353974683068ef5f
beng.lim@latitudefinancial.com <beng.lim@latitudefinancial.com>
2020-01-15 00:25:52 +1100

    added step7

--- provider/product/product.pact.test.js
+++ provider/product/product.pact.test.js
@@ -1,5 +1,7 @@
 const { Verifier } = require('@pact-foundation/pact');
 const path = require('path');
+const controller = require('./product.controller');
+const Product = require('./product');
 
 // Setup provider server to verify
 const app = require('express')();
@@ -15,7 +17,26 @@ describe("Pact Verification", () => {
             providerVersion: "1.0.0",
             pactUrls: [
                 path.resolve(__dirname, '../pacts/frontendwebsite-productservice.json')
-            ]
+            ],
+            stateHandlers: {
+                "product with ID 10 exists": () => {
+                    controller.repository.products = new Map([
+                        ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+                    ]);
+                },
+                "products exist": () => {
+                    controller.repository.products = new Map([
+                        ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+                        ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+                    ]);
+                },
+                "no products exist": () => {
+                    controller.repository.products = new Map();
+                },
+                "product with ID 11 does not exist": () => {
+                    controller.repository.products = new Map();
+                },
+            }
         };
 
         return new Verifier(opts).verifyProvider().finally(() => {
