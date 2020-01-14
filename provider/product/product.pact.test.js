c6faf2fdb969a6511b7761181d7ea8a8803c2033
beng.lim@latitudefinancial.com <beng.lim@latitudefinancial.com>
2020-01-15 00:15:17 +1100

    added step 4

---
+++ provider/product/product.pact.test.js
@@ -0,0 +1,25 @@
+const { Verifier } = require('@pact-foundation/pact');
+const path = require('path');
+
+// Setup provider server to verify
+const app = require('express')();
+app.use(require('./product.routes'));
+const server = app.listen("8080");
+
+describe("Pact Verification", () => {
+    it("validates the expectations of ProductService", () => {
+        let opts = {
+            logLevel: "INFO",
+            providerBaseUrl: "http://localhost:8080",
+            provider: "ProductService",
+            providerVersion: "1.0.0",
+            pactUrls: [
+                path.resolve(__dirname, '../pacts/frontendwebsite-productservice.json')
+            ]
+        };
+
+        return new Verifier(opts).verifyProvider().finally(() => {
+            server.close();
+        });
+    })
+});
