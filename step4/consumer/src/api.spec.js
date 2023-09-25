73aeb3919ae0f6c1a29b8dc809c85c4b981f8f1b
Yousaf Nabi <yousaf@pactflow.io>
2023-09-25 13:13:14 +0100

    add step4

---
+++ step4/consumer/src/api.spec.js
@@ -0,0 +1,43 @@
+import API from "./api";
+import nock from "nock";
+
+describe("API", () => {
+
+    test("get all products", async () => {
+        const products = [
+            {
+                "id": "9",
+                "type": "CREDIT_CARD",
+                "name": "GEM Visa",
+                "version": "v2"
+            },
+            {
+                "id": "10",
+                "type": "CREDIT_CARD",
+                "name": "28 Degrees",
+                "version": "v1"
+            }
+        ];
+        nock(API.url)
+            .get('/products')
+            .reply(200,
+                products,
+                {'Access-Control-Allow-Origin': '*'});
+        const respProducts = await API.getAllProducts();
+        expect(respProducts).toEqual(products);
+    });
+
+    test("get product ID 50", async () => {
+        const product = {
+            "id": "50",
+            "type": "CREDIT_CARD",
+            "name": "28 Degrees",
+            "version": "v1"
+        };
+        nock(API.url)
+            .get('/products/50')
+            .reply(200, product, {'Access-Control-Allow-Origin': '*'});
+        const respProduct = await API.getProduct("50");
+        expect(respProduct).toEqual(product);
+    });
+});
---
+++ step4/provider/product/product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, type, name, version) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.version = version
+    }
+}
+
+module.exports = Product;
