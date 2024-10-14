de0f0dd97daba7aa19d1d461175a09c26f053f7d
Yousaf Nabi <you@saf.dev>
2024-10-14 19:49:37 +0100

    ci: test

--- broker-webhook/create_webhook.sh
+++ broker-webhook/create_webhook.sh
@@ -1,3 +1,28 @@
-curl http://localhost:8000/webhooks \
-    -X POST --user pact_workshop:pact_workshop \
-    -H "Content-Type: application/json" -d @broker-create-body.json -v
+#!/bin/bash
+set -e
+
+json_payload='{
+    "events": [
+      {
+        "name": "contract_content_changed"
+      }
+    ],
+    "request": {
+      "method": "POST",
+      "url": "'http://"${PACT_BROKER_WEBHOOK_HOST_WHITELIST:-host.docker.internal}":9090'",
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "body": {
+        "state": "${pactbroker.githubVerificationStatus}",
+        "description": "Pact Verification Tests ${pactbroker.providerVersionTags}",
+        "context": "${pactbroker.providerName}",
+        "target_url": "${pactbroker.verificationResultUrl}"
+      }
+    }
+  }'
+
+echo $json_payload
+curl http://localhost:8000/webhooks/204430af-f8c8-4feb-8135-f027df8ecf34 \
+    -X PUT --user pact_workshop:pact_workshop \
+    -H "Content-Type: application/json" -d "${json_payload}" -v
